18a02d092d9cd0ed8a6ca24d99ce89dd22d20dc0
unknown <kamil.nawrocki5@gmail.com>
2018-05-30 13:28:37 +0200

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+// Heroku sets process.env.PORT variable
+const port = process.env.PORT || 3000;
+const app = express();
+
+// Serves static assets
+app.use(express.static(publicPath));
+
+app.listen(3000, () => {
+	console.log(`Started on port ${port}`);
+})
