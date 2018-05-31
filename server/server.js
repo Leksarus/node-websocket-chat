c124f731c5bfa04deba1686ecd7be6e8089b35fa
unknown <kamil.nawrocki5@gmail.com>
2018-05-31 20:31:14 +0200

    Emit messages/

--- server/server.js
+++ server/server.js
@@ -16,11 +16,31 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New user connected');
 
+	socket.emit('newMessage', {
+		from: 'exampleUser1',
+		text: 'Lore ipsum dlor sit amet',
+		createdAt: new Date()
+	});
+
+	socket.on('createMessage', (message) => {
+		console.log(message);
+
+		// io emits event to everybody
+		io.emit('newMessage', {
+			from: message.from,
+			text: message.text,
+			createdAt: new Date().getTime()
+		})
+	});
+
 	socket.on('disconnect', () => {
 		console.log('User disconnected');
-	})
+	});
 });
 
 server.listen(port, () => {
 	console.log(`Started on port ${port}`);
 })
+
+// socket.emit emits event to single connection
+// io.emit emits event to every connection
