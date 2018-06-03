08edfaa29defae07d8e29047072f2dd64eb553ce
unknown <kamil.nawrocki5@gmail.com>
2018-06-03 13:49:33 +0200

    Create message generator utility.

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 // Heroku sets process.env.PORT variable
 const port = process.env.PORT || 3000;
@@ -16,21 +17,22 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New user connected');
 
-	socket.emit('newMessage', {
-		from: 'exampleUser1',
-		text: 'Lore ipsum dlor sit amet',
-		createdAt: new Date()
-	});
+	socket.emit('newMessage', generateMessage('Admin', 'Welcome in channel'));
+
+	socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
 	socket.on('createMessage', (message) => {
 		console.log(message);
 
 		// io emits event to everybody
-		io.emit('newMessage', {
+		io.emit('newMessage', generateMessage(message.from, message.text));
+
+		// if I send event, broadcast will send message to everybody but me
+		/*socket.broadcast.emit('newMessage', {
 			from: message.from,
 			text: message.text,
-			createdAt: new Date().getTime()
-		})
+			createAt: new Date().getTime()
+		})*/
 	});
 
 	socket.on('disconnect', () => {
@@ -44,3 +46,4 @@ server.listen(port, () => {
 
 // socket.emit emits event to single connection
 // io.emit emits event to every connection
+// socket.broadcast send event to everybody but source event maker
